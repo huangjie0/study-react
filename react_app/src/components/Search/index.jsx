00a08195774b810d6489b2005cdeefb93552d277
huangjie4 <1364045672@qq.com>
2024-09-19 12:13:20 +0800

    修改

--- react_app/src/components/Search/index.jsx
+++ react_app/src/components/Search/index.jsx
@@ -7,10 +7,13 @@ export default class Search extends Component {
       <div className="box2">
           <div className="box3">Search Github Users</div>
           <div className="box4">
-              <input type="text" className="input" placeholder="请输入要搜索的内容呢"/>
-              <button className="btn">Search</button>
+              <input ref={ c => this.inputRef = c }  type="text" className="input" placeholder="请输入要搜索的内容呢"/>
+              <button className="btn" onClick = { this.search }>Search</button>
           </div>
       </div>
     )
+  }
+  search = ()=>{
+    console.log(this.inputRef.value);
   }
 }
