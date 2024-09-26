0d44cd858c6e175b8e9ab737984f1a1c4eda1831
huangjie4 <1364045672@qq.com>
2024-09-26 10:55:35 +0800

    将ui组件和容器组件整合

--- react_app/src/containers/Count/index.jsx
+++ react_app/src/containers/Count/index.jsx
@@ -1,9 +1,74 @@
-import CountUi from "../../components/Count";
+import React, { Component } from 'react'
+import { Select } from 'antd'
 import { connect } from 'react-redux';
 import { createAddAction , createSubtractionAction , createAsyncAddAction } from '../../redux/count_action'
 
-const mapStateToProps = state => ({count:state})
+class Count extends Component {
+    state = {
+        number:1
+    }
+    handleChange = (value)=>{
+        this.setState({
+            number:value
+        })
+    }
+    // 加
+    add = ()=>{
+        const { number } = this.state
+        this.props.add(number)
+    }
+    // 减
+    subtraction = ()=>{
+        const { number } = this.state
+        this.props.subtraction(number)
+    }
+    // 奇数就加
+    odd = ()=>{
+        const { number } = this.state
+        if(this.props.count % 2 ===0 ) return
+        this.props.add(number)
+    }
 
+    asynchronous = ()=>{
+        const { number } = this.state
+        this.props.asyncAdd(number,500)
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>当前求和为：{ this.props.count }</h1>
+            <Select
+                defaultValue="1"
+                style={{
+                    width: 120,
+                }}
+                onChange={this.handleChange}
+                options={[
+                    {
+                        value: 1,
+                        label: '1'
+                    },
+                    {
+                        value: 2,
+                        label: '2'
+                    },
+                    {
+                        value: 3,
+                        label: '3'
+                    }
+                ]}
+            />
+            <button onClick={ this.add }>+</button>
+            <button onClick={  this.subtraction}>-</button>
+            <button onClick={ this.odd}>当前求和为奇数加</button>
+            <button onClick={this.asynchronous}>异步加</button>
+        </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({count:state})
 const mapDispatchToProps = dispatch => (
     {
         add:(number)=>{
@@ -17,7 +82,6 @@ const mapDispatchToProps = dispatch => (
         }
     }
 )
-
-export default connect(mapStateToProps,mapDispatchToProps)(CountUi)
+export default connect(mapStateToProps,mapDispatchToProps)(Count)
 
 
