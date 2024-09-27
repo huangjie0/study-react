ac18744219a06b8545a6c146fc35c889695276e8
huangjie4 <1364045672@qq.com>
2024-09-27 09:01:35 +0800

    修改

--- react_app/src/routes/index.js
+++ react_app/src/routes/index.js
@@ -1,31 +1,39 @@
-import Home from '../pages/Home'
-import About from '../pages/About'
-import News from '../pages/Home/News'
-import Message from '../pages/Home/Message'
-import Detail from '../pages/Home/Message/Detail'
+import { lazy,Suspense } from 'react'
 import { Navigate } from 'react-router-dom'
+
+const About = lazy(()=>import('../pages/About'))
+const Home = lazy(()=>import('../pages/Home'))
+const News = lazy(()=>import('../pages/Home/News'))
+const Message = lazy(()=>import('../pages/Home/Message'))
+const Detail = lazy(()=>import('../pages/Home/Message/Detail'))
+
+const withLoadingComponent = (comp) => (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      {comp}
+    </Suspense>
+)
 
 const routes =  [
     {
         path:'/about',
-        element:<About/>
+        element:withLoadingComponent(<About/>)
     },
     {
         path:'/home',
-        element:<Home/>,
+        element:withLoadingComponent(<Home/>),
         children:[
             {
                 path:'news',
-                element:<News/>
+                element:withLoadingComponent(<News/>)
             },
             {
                 path:'message',
-                element:<Message/>,
+                element:withLoadingComponent(<Message/>),
                 children:[
                     {
                         // path:'detail/:id/:title/:content',
                         path:'detail',
-                        element:<Detail/>
+                        element:withLoadingComponent(<Detail/>)
                     }
                 ]
             }
