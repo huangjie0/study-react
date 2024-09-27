025d29f3a84c6f496aac6a20e7eed1f4c3ca53ae
huangjie4 <1364045672@qq.com>
2024-09-27 15:46:39 +0800

    修改

--- react_app/src/components/Context.jsx
+++ react_app/src/components/Context.jsx
@@ -16,7 +16,7 @@ export default class Context extends PureComponent {
             <h1>我是context组件</h1>
             <h4>用户名是：{ username }</h4>
             <Provider value={{ username , age}}>
-                <B/>  
+                <B render={(a)=><C a={ a }/>}/>
             </Provider>
         </div>
     )
@@ -25,43 +25,49 @@ export default class Context extends PureComponent {
 
 class B extends PureComponent{
     static contextType = MyContext
+    
+    state = {
+        a:'纳米哈'
+    }
     render(){
+        const { a } = this.state
         return(
             <div>
                 <h1>我是B组件</h1>
                 <h4>从Context组件接受到的用户名是：{ this.context.username }</h4>
-                <C/>
+                { this.props.render(a)}
             </div>
         )
     }
 }
 
-// class C extends Component{
-//     static contextType = MyContext
-//     render(){
-//         console.log(this.context);
-//         return(
-//             <div>
-//                 <h1>我是C组件</h1>
-//                 <h4>从Context组件接受到的用户名是：{ this.context.username }</h4>
-//             </div>
-//         )
-//     }
-// }
-
-function C (){
-    return (
-        <div>
-            <h1>我是C组件</h1>
-            <h4>从Context组件接受到的用户名是：
-                <Consumer>
-                    {
-                        value =>{
-                            return `${value.username},age是${value.age}`
-                        }
-                    }
-                </Consumer>
-            </h4>
-        </div>
-    )
+class C extends PureComponent{
+    static contextType = MyContext
+    render(){
+        const { a } = this.props;
+        return(
+            <div>
+                <h1>我是C组件</h1>
+                <h4>从Context组件接受到的用户名是：{ this.context.username }</h4>
+                <h2>{ a }</h2>
+            </div>
+        )
+    }
 }
+
+// function C (){
+//     return (
+//         <div>
+//             <h1>我是C组件</h1>
+//             <h4>从Context组件接受到的用户名是：
+//                 <Consumer>
+//                     {
+//                         value =>{
+//                             return `${value.username},age是${value.age}`
+//                         }
+//                     }
+//                 </Consumer>
+//             </h4>
+//         </div>
+//     )
+// }
